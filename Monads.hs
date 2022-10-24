b6b6377e37be26f7e0495f6111d41add495763bf
Ernest Ng <ngernest@sas.upenn.edu>
2022-10-24 12:06:35 -0400

    Exercise solutions

--- Monads.hs
+++ Monads.hs
@@ -7,8 +7,8 @@ fulltitle: The Maybe and List Monads
 module Monads where
 
 import Control.Monad (guard)
-import qualified Data.List as List
-import qualified Data.Maybe as Maybe
+import Data.List qualified as List
+import Data.Maybe qualified as Maybe
 import Prelude hiding ((>>))
 
 {-
@@ -29,7 +29,10 @@ in the tree.
 
 -- | zip two trees together
 zipTree :: Tree a -> Tree b -> Tree (a, b)
-zipTree = undefined
+zipTree (Leaf x) (Leaf y) = Leaf (x, y)
+zipTree (Branch xl xr) (Branch yl yr) =
+  Branch (zipTree xl yl) (zipTree xr yr)
+zipTree _ _ = error "input trees must have the same shape"
 
 {-
         o                     o                      o
@@ -65,7 +68,14 @@ Let's rewrite it so that the partiality is explicit in the type.
 -}
 
 zipTree1 :: Tree a -> Tree b -> Maybe (Tree (a, b))
-zipTree1 = undefined
+zipTree1 (Leaf x) (Leaf y) = Just $ Leaf (x, y)
+zipTree1 (Branch xl xr) (Branch yl yr) =
+  case (zipTree1 xl yl, zipTree1 xr yr) of
+    (Just t1, Just t2) -> Just $ Branch t1 t2
+    (_, Nothing) -> Nothing
+    (Nothing, _) -> Nothing
+zipTree1 (Leaf _) (Branch _ _) = Nothing
+zipTree1 (Branch _ _) (Leaf _) = Nothing
 
 {-
 This function is going to be our inspiration for the following development.
@@ -380,7 +390,9 @@ zipTree function using the above `do` notation for the `>>=` operator.
 zipTree5 :: Tree a -> Tree b -> Maybe (Tree (a, b))
 zipTree5 (Leaf a) (Leaf b) = return (Leaf (a, b))
 zipTree5 (Branch l r) (Branch l' r') = do
-  undefined
+  m1 <- zipTree5 l l'
+  m2 <- zipTree5 r r'
+  return $ Branch m1 m2
 zipTree5 _ _ = Nothing
 
 -- >>> testZip zipTree5
@@ -534,13 +546,37 @@ right. )
 -}
 
 fmapMonad :: (Monad m) => (a -> b) -> m a -> m b
-fmapMonad = undefined
+fmapMonad f m = m >>= return . f
+
+{-
+
+liftM   :: (Monad m) => (a1 -> r) -> m a1 -> m r
+liftM f m1 = do
+  x1 <- m1
+  return (f x1)
+
+liftM f m1 = m1 >>= (\x1 -> f x1)
+f :: a1 -> r
+m1 :: m a1
+x1 :: a1
+f x1 :: r
+
+-}
 
 pureMonad :: (Monad m) => a -> m a
-pureMonad = undefined
+pureMonad = return
 
 zapMonad :: (Monad m) => m (a -> b) -> m a -> m b
-zapMonad = undefined
+zapMonad m1 m2 = m1 >>= \f -> m2 >>= \x -> return (f x)
+
+{-
+m1 :: m (a -> b)
+f :: a -> b
+m2 :: m a
+x :: a
+f x :: b
+return f x :: m b
+-}
 
 {-
 Note that `fmapMonad` is called `liftM` and `zapMonad` is called `ap` in the
@@ -582,7 +618,7 @@ should *not* be recursive.
 -- >>> pairs0 [1,2,3] [5,6,7]
 -- [(1,5),(1,6),(1,7),(2,5),(2,6),(2,7),(3,5),(3,6),(3,7)]
 pairs0 :: [a] -> [b] -> [(a, b)]
-pairs0 xs ys = undefined
+pairs0 xs ys = concat $ map (\x -> concat $ map (\y -> [(x, y)]) ys) xs
 
 testPairs :: ([Int] -> [Int] -> [(Int, Int)]) -> Bool
 testPairs ps =
@@ -707,8 +743,14 @@ Rewrite `pairs` using `>>=` and return
 
 -- >>> pairs2 [1,2,3] [5,6,7]
 -- [(1,5),(1,6),(1,7),(2,5),(2,6),(2,7),(3,5),(3,6),(3,7)]
+
 pairs2 :: [a] -> [b] -> [(a, b)]
-pairs2 xs ys = undefined
+pairs2 xs ys =
+  xs
+    >>= ( \x ->
+            ys
+              >>= \y -> return (x, y)
+        )
 
 {-
 Rewrite again using do notation
@@ -717,7 +759,10 @@ Rewrite again using do notation
 -- >>> pairs3 [1,2,3] [5,6,7]
 -- [(1,5),(1,6),(1,7),(2,5),(2,6),(2,7),(3,5),(3,6),(3,7)]
 pairs3 :: [a] -> [b] -> [(a, b)]
-pairs3 xs ys = undefined
+pairs3 xs ys = do
+  x <- xs
+  y <- ys
+  return (x, y)
 
 {-
 Make sure that it still works.
@@ -832,6 +877,7 @@ And this code finds the smallest list of colors that can do so:
 -}
 
 -- >>> colorsNeeded
+-- Just [Red,Green,Blue]
 colorsNeeded :: Maybe [Color]
 colorsNeeded = List.find (not . null . stateColors) cs
   where
@@ -845,9 +891,10 @@ Other examples
 * Rewrite the `map` function using a list comprehension.
 -}
 
--- >>> map' (+1) [1,2,3]
+-- >>> map1 (+1) [1,2,3]
+
 map' :: (a -> b) -> [a] -> [b]
-map' f xs = undefined
+map' f xs = [f x | x <- xs]
 
 {-
 * Create a list of all pairs where the first component is from the first list,
@@ -856,25 +903,38 @@ map' f xs = undefined
 -}
 
 -- >>> firstLess [1,2,3] [1,2,3]
+-- [(1,2),(1,3),(2,3)]
 firstLess :: Ord a => [a] -> [a] -> [(a, a)]
-firstLess = undefined
+firstLess xs ys = [(x, y) | x <- xs, y <- ys, x < y]
 
 {-
 Now rewrite `map'` and `firstLess` using do notation (don't forget `guard` above)
 -}
 
 map1 :: (a -> b) -> [a] -> [b]
-map1 = undefined
+map1 f xs = do
+  x <- xs
+  return (f x)
+
+{-
+Equivalent to:
+map2 :: (a -> b) -> [a] -> [b]
+map2 f xs = xs >>= (\x -> return $ f x)
+-}
 
 firstLess1 :: Ord a => [a] -> [a] -> [(a, a)]
-firstLess1 xs ys = undefined
+firstLess1 xs ys = do
+  x <- xs
+  y <- xs
+  guard (x < y)
+  return (x, y)
 
 {-
 * Rewrite `filter`, using a guarded list comprehension.
 -}
 
 filter' :: (a -> Bool) -> [a] -> [a]
-filter' f xs = undefined
+filter' f xs = [x | x <- xs, f x]
 
 {-
 The List Applicative
@@ -898,8 +958,17 @@ at the definition above to see what could makes sense.
 -}
 
 -- >>> pairs6 [1,2,3] [1,2,3]
+-- [(1,1),(1,2),(1,3),(2,1),(2,2),(2,3),(3,1),(3,2),(3,3)]
 pairs6 :: [a] -> [b] -> [(a, b)]
-pairs6 xs ys = undefined
+pairs6 xs ys = pure (,) <*> xs <*> ys
+
+{-
+pairs6 xs ys
+== [(,)] <*> xs <*> ys
+== [(,) x | x <- xs] <*> ys
+== [(x,) | x <- xs] <*> ys
+== [(x,y) | x <- xs, y <- ys]
+-}
 
 {-
 Once you get `pairs6`, try inlining the definitions of `pure` and `<*>` to see
